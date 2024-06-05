098fed9ff7f97d73e830672361ce050f812bba61
KIMDONGHWE <kys8634kys8634@gmail.com>
2024-06-05 14:08:53 +0900

    login&join_v2

---
+++ src/main/java/com/demo/Web/controller/JoinController.java
@@ -0,0 +1,9 @@
+package com.demo.Web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller("join") // Controller 역할을 하는 클래스
+public class JoinController {
+}
